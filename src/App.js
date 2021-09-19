a2ddd2d9b9c04711f6052b686a7f6cc6e58d5702
henni saaranen <n0sahe00@students.oamk.fi>
2021-09-19 18:04:55 +0300

    vie heart_rate githubiin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [age, setAge] = useState(0);
+  const [rate, setRate] = useState(0);
+  const [upper, setUpper] = useState(0);
+  const [lower, setLower] = useState(0);
+
+  function calculate(e) {
+    e.preventDefault();
+    setUpper((220 - age) * 0.85);
+    setLower((220 - age) * 0.65);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={calculate}>
+
+      <h3>Heart rate limits calculator</h3>
+      <div>
+        <label>Age</label>
+        <input type="number" value={age} onChange={e => setAge(e.target.value)}></input>
+      </div>
+      <div>
+        <label>Heart rate limits</label>
+          <output>{lower} - {upper}</output>
+      </div>
+      <div>
+        <button>Calculate</button>
+      </div>
+    </form>
   );
 }
 
